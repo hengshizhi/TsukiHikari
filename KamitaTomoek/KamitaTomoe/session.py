6895eb8cf65107ab3cf4a9776277aa56409f2338
星姮十织 <87295882+hengshizhi@users.noreply.github.com>
2023-01-25 18:53:32 +0800

    修复session无法使用的bug

--- KamitaTomoek/KamitaTomoe/session.py
+++ KamitaTomoek/KamitaTomoe/session.py
@@ -1,11 +1,11 @@
-from flask import session
+from flask import session as flaskse
 import os
 _root = None #项目根路径
 File_path = None #文件路径
+session = flaskse #缓存已经修改的session
 def getdict() -> dict:
     '''获取所有session'''
-    r = session
-    return dict(r)
+    return dict(session)
 def sessions(s:str):
     '''获取session'''
     try:r = session[s]
@@ -15,3 +15,9 @@ def modify(m:dict):
     '''修改session'''
     os.chdir(_root)
     for k,v in m.items():session[k] = v
+def move(): #到代码运行结尾运行move修改flask上的session
+    '''运行session修改,注意(很重要):'''
+    os.chdir(_root)
+    for k,v in session.items():
+        flaskse[k] = v
+    session = {}
